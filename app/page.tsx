1bd0973102224bb2ec11c272c20b1887c58c8be3
FrBosquet <frbosquet@gmail.com>
2024-02-10 17:53:07 +0100

    fix 404

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function NotFoundPage() {
-  return <section className="flex h-screen flex-col items-center justify-center gap-8">
-    <h1 className="font-script text-9xl font-semibold">¡Oopsie!</h1>
-    <p className="text-sm">NO TE HEMOS ENCONTRADO ENTRE LA LISTA DE INVITADOS. ¿PODRÍAS REVISAR TU ENLACE?</p>
+  return <section className="flex flex-col items-center justify-center gap-8 text-zinc-300 size-screen">
+    <h1 className="font-dancing text-6xl font-semibold md:text-9xl">¡Oopsie!</h1>
+    <p className="max-w-80 text-center text-md">No te hemos encontrado entre la lista de invitados. ¿podrías revisar tu enlace?</p>
   </section>
 }
