b8183d8739297772a4044a92cbadaaba8f4bda01
FrBosquet <21219312+FrBosquet@users.noreply.github.com>
2024-09-01 20:26:25 +0200

    Enhance DX

--- app/private/(regular)/preferences/page.tsx
+++ app/private/(regular)/preferences/page.tsx
@@ -1,19 +1,27 @@
-import { Select } from "@/components/form/select";
-import { Languages } from "lucide-react";
+import { Languages } from 'lucide-react'
+
+import { Select } from '@/components/form/select'
 
 export default async function PrivatePage() {
   return (
     <section className="flex flex-col gap-4">
-      <h2 className="font-sans text-xl uppercase flex items-center gap-2 border-b-2"><Languages /> Idioma</h2>
+      <h2 className="flex items-center gap-2 border-b-2 font-sans text-xl uppercase">
+        <Languages /> Idioma
+      </h2>
 
-      <Select values={[{
-        value: 'en',
-        label: 'English'
-      }, {
-        value: 'es',
-        label: 'Español'
-      }]} />
-
+      <Select
+        name="language"
+        values={[
+          {
+            value: 'en',
+            label: 'English'
+          },
+          {
+            value: 'es',
+            label: 'Español'
+          }
+        ]}
+      />
     </section>
   )
 }
