1d7eb660d8809308418224c5d6ec4020e6f16460
FrBosquet <21219312+FrBosquet@users.noreply.github.com>
2024-09-22 10:20:16 +0200

    Fix tooltip translation

--- components/avatar/profile.tsx
+++ components/avatar/profile.tsx
@@ -13,16 +13,16 @@ type Props = {
 }
 
 const Profile = ({ userOnEvent, itsYou }: Props) => {
-  const { intl } = useIntl()
+  const { t } = useIntl()
   const { user, email, createdAt: invitationCreatedAt } = userOnEvent
 
   if (!user)
     return (
       <div className="flex w-36 flex-col items-center gap-2">
-        <p>{intl('dashboard.event.invite.invited')}</p>
+        <p>{t('dashboard.event.invite.invited')}</p>
         <p className="w-full text-slate-400">{email}</p>
         <p className="w-full font-sans text-xs">
-          {intl('dashboard.event.invite.date', {
+          {t('dashboard.event.invite.date', {
             date: { date: invitationCreatedAt }
           })}
         </p>
@@ -35,12 +35,12 @@ const Profile = ({ userOnEvent, itsYou }: Props) => {
     <div className="flex w-36 flex-col items-center gap-2">
       <AvatarChip className="size-24" userOnEvent={userOnEvent} />
       <h2 className="w-full text-lg font-semibold">
-        {user.name} {itsYou && ' (tu)'}
+        {user.name} {itsYou && ` (${t('you')})`}
       </h2>
       <p className="text-xs uppercase"></p>
       <p className="w-full text-slate-400">{user.email}</p>
       <p className="w-full font-sans text-xs">
-        {intl('dashboard.event.invite.since', {
+        {t('dashboard.event.invite.since', {
           date: { date: userCreatedAt }
         })}
       </p>
